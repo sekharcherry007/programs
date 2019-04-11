7096aa2502c4c8b1423fcfe3fac667e5c6a8a1de
sekharcherry007 <49332961+sekharcherry007@users.noreply.github.com>
2019-04-11 08:09:17 +0530

    multiplication of two numbers

---
+++ multiplication.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter number a:");
+	scanf("%d",&a);
+	printf("enter number b:");
+	scanf("%d",&b);
+	c=a*b;
+	printf("multiplication is:%d",c);
+}
