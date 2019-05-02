2ccdeb4cf71022250c5fa767a6bd6393160b45bd
sekharcherry007 <49332961+sekharcherry007@users.noreply.github.com>
2019-05-02 18:05:44 +0530

    divisble by 5

---
+++ divisble by 5.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+	int n;
+	printf("enter the number:");
+	scanf("%d",&n);
+	if(n%5==0)
+	printf("number is divisble by 5");
+	else
+	printf("number is not divisble by 5");
+}
