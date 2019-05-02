06af773f2073637744b8caf2eea25c9350ad626c
sekharcherry007 <49332961+sekharcherry007@users.noreply.github.com>
2019-05-02 18:09:27 +0530

    multiple by 7

---
+++ multiple by 7.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+	int n;
+	printf("enter the number:");
+	scanf("%d",&n);
+	if(n%7==0)
+	printf("number is multiple by 7");
+	else
+	printf("number is not multiple by 7");
+}
