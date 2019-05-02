b91f693971d55cab5637cced2ad0da832baffb92
sekharcherry007 <49332961+sekharcherry007@users.noreply.github.com>
2019-05-02 17:38:44 +0530

    largest value

---
+++ largest value.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+	int a,b;
+	printf("enter the values:");
+	scanf("%d%d",&a,&b);
+	if(a>b)
+	printf("largest value is %d",a);
+	else
+	printf("largest value is %d",b);
+}
