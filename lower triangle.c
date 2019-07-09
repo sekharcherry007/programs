d792ae0000dfa53393ebed5d3eef80d0698fb8c6
sekharcherry007 <49332961+sekharcherry007@users.noreply.github.com>
2019-07-09 11:04:57 +0530

    lower triangle

---
+++ lower triangle.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+main()
+{
+	int i,j,k,n;
+	printf("enter a number:");
+	scanf("%d",&n);
+	for(i=n;i>=1;i--)
+	{
+		for(k=1;k<(n-i);k++)
+		{
+			printf(" ");
+		}
+		for(j=1;j<=i;j++)
+		{
+			printf("*");
+		}
+		for(j=2;j<=i;j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+}
