b6831a46a85030d25833c582c654bea82e0f2efb
sekharcherry007 <49332961+sekharcherry007@users.noreply.github.com>
2019-07-09 11:00:49 +0530

    daimond shape pattern

---
+++ diamond shape.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+main()
+{
+	int i,j,k,n;
+	printf("enter a number:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		for(k=1;k<(n-i);k++)
+		{
+			printf(" ");
+		}
+		for(j=1;j<=i;j++)
+		{
+			printf("*");
+		}
+		for(j=2;j<=i;j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	for(i=n;i>=1;i--)
+	{
+		for(k=1;k<(n-i);k++)
+		{
+			printf(" ");
+		}
+		for(j=1;j<=i;j++)
+		{
+			printf("*");
+		}
+		for(j=2;j<=i;j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+}
